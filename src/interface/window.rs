b4a6da80fe6916df650187e73a23acfd3c9af007
ggemre <gmoore1@byu.edu>
2023-12-09 18:14:48 -0700

    feat(interface): selection ui

--- src/interface/window.rs
+++ src/interface/window.rs
@@ -25,15 +25,17 @@ fn get_window() -> &'static mut Window {
 }
 
 fn draw(window: &Window, model: &Vec<Widget>) {
+    let left_margin: i32 = 2;
+    
     window.clear();
     
     for widget in model {
         match widget {
             Widget::Input { y, max_width, filter, label, placeholder, content } => {
-                window.mvprintw(*y as i32, 0, &content);
+                window.mvprintw(*y as i32, left_margin, &content);
             }
             Widget::Text { y, content } => {
-                window.mvprintw(*y as i32, 0, &content);
+                window.mvprintw(*y as i32, left_margin, &content);
             }
         }
     }
@@ -44,6 +46,7 @@ fn wait_for_input(window: &Window, model: &mut Vec<Widget>) -> (BreakCondition, 
     let mut cursor = Cursor { x: 0, y: 0 };
     let break_condition: BreakCondition;
     let limit = model.len() - 1;
+    let left_margin: usize = 2;
 
     if let Some(Widget::Input { content, .. }) = model.get(cursor.y) {
         cursor.x = content.len();
@@ -52,8 +55,6 @@ fn wait_for_input(window: &Window, model: &mut Vec<Widget>) -> (BreakCondition, 
     window.mv(cursor.y as i32, cursor.x as i32);
 
     loop {
-        window.refresh();
-        
         let ch = window.getch();
         match ch {
             Some(Input::Character('\u{1b}')) => {
@@ -71,41 +72,43 @@ fn wait_for_input(window: &Window, model: &mut Vec<Widget>) -> (BreakCondition, 
             }
             Some(Input::KeyUp) => {
                 if cursor.y > 0 {
+                    window.mvprintw(cursor.y as i32, 0, " ");
                     cursor.y -= 1;
 
                     if let Some(Widget::Input { content, .. }) = model.get(cursor.y) {
+                        curs_set(1);
                         cursor.x = content.len();
                     } else {
-                        cursor.x = 0;
+                        curs_set(0);
+                        window.mvprintw(cursor.y as i32, 0, ">");
                     }
-
-                    window.mv(cursor.y as i32, cursor.x as i32);
                 }
             }
             Some(Input::KeyDown) => {
                 if cursor.y < limit {
+                    window.mvprintw(cursor.y as i32, 0, " ");
                     cursor.y += 1;
 
                     if let Some(Widget::Input { content, .. }) = model.get(cursor.y) {
+                        curs_set(1);
                         cursor.x = content.len();
                     } else {
-                        cursor.x = 0;
+                        curs_set(0);
+                        window.mvprintw(cursor.y as i32, 0, ">");
                     }
 
-                    window.mv(cursor.y as i32, cursor.x as i32);
+                    // window.mv(cursor.y as i32, cursor.x as i32);
                 }
             }
             Some(Input::KeyLeft) => {
                 if cursor.x > 0 && matches!(model.get(cursor.y), Some(Widget::Input { .. })) {
                     cursor.x -= 1;
-                    window.mv(cursor.y as i32, cursor.x as i32);
                 }
             }
             Some(Input::KeyRight) => {
                 if let Some(Widget::Input { content, .. }) = model.get(cursor.y) {
                     if cursor.x < content.len() {
                         cursor.x += 1;
-                        window.mv(cursor.y as i32, cursor.x as i32);
                     }
                 }
             }
@@ -114,7 +117,7 @@ fn wait_for_input(window: &Window, model: &mut Vec<Widget>) -> (BreakCondition, 
             Some(Input::Character('\u{7f}')) => {
                 if cursor.x > 0 {
                     cursor.x -= 1;
-                    window.mv(cursor.y as i32, cursor.x as i32);
+                    window.mv(cursor.y as i32, (cursor.x + left_margin) as i32);
                     window.delch();
                     
                     if let Some(widget) = model.get_mut(cursor.y) {
@@ -126,9 +129,9 @@ fn wait_for_input(window: &Window, model: &mut Vec<Widget>) -> (BreakCondition, 
             }
             Some(Input::Character(c)) => {
                 if let Some(Widget::Input { content, .. }) = model.get(cursor.y) {
+                    window.mvinsch(cursor.y as i32, (cursor.x + left_margin) as i32, c as chtype);
                     cursor.x += 1;
-                    window.mvinsch(cursor.y as i32, (cursor.x - 1) as i32, c as chtype);
-                    window.mv(cursor.y as i32, cursor.x as i32);
+                    // window.mv(cursor.y as i32, cursor.x as i32);
 
                     if let Some(widget) = model.get_mut(cursor.y) {
                         if let Widget::Input { content, .. } = widget {
@@ -139,6 +142,9 @@ fn wait_for_input(window: &Window, model: &mut Vec<Widget>) -> (BreakCondition, 
             }
             _ => {}
         }
+
+        window.mv(cursor.y as i32, (cursor.x + left_margin) as i32);
+        window.refresh();
     }
 
     return (break_condition, cursor.y);
@@ -150,6 +156,7 @@ pub fn init(model: &mut Vec<Widget>) -> (BreakCondition, usize) {
     window.nodelay(true);
     window.timeout(0);
     noecho();
+    curs_set(1);
 
     draw(&window, &model);
     return wait_for_input(&window, model);
